bec54808d3f6ad2d693f3cfba755e658f219fefa
juanp <juanp@DESKTOP-JATAE0K>
2022-09-04 21:46:30 -0600

    Menu principal de prueba agregado

---
+++ Controllers/MenuBasicoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPeluqueria.Controllers
+{
+    public class MenuBasicoController : Controller
+    {
+        // GET: MenuBasico
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
